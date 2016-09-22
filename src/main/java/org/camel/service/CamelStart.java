14cb89a5e13db97ab61925907de2cd0ae5e1dc5e
Swapnil Adhav <Swapnil_Adhav@gap.com>
2016-09-22 18:17:39 +0530

    again me

--- src/main/java/org/camel/service/CamelStart.java
+++ src/main/java/org/camel/service/CamelStart.java
@@ -8,6 +8,7 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.processor.ThrowExceptionProcessor;
 import org.camel.router.IntegrationRoute;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,27 +23,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CamelStart {
 
-    private static final Logger logger = LoggerFactory.getLogger(CamelStart.class);
+	private static final Logger logger = LoggerFactory.getLogger(CamelStart.class);
 
-    /**
-     * Simply selects the home view to render by returning its name.
-     */
-    @RequestMapping(value = "/copy-files", method = RequestMethod.GET)
-    public String home(Locale locale, Model model) {
-        logger.info("Welcome home! The client locale is {}.", locale);
-        try {
-            CamelContext context = new DefaultCamelContext();
-            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
-            context.addComponent("activemq", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
-            context.addRoutes(new IntegrationRoute());
-            context.start();
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/copy-files", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		try {
+			CamelContext context = new DefaultCamelContext();
+			ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+			context.addComponent("activemq", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+			context.addRoutes(new IntegrationRoute());
+			context.start();
 
-            Thread.sleep(3000);
-            context.stop();
-        } catch (Exception err) {
-            err.printStackTrace();
-        }
-        return "home";
-    }
+			Thread.sleep(3000);
+			context.stop();
+		} catch (Throwable err) {
+			err.printStackTrace();
+		}
+		return "home";
+	}
 
 }
