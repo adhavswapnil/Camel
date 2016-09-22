caada65ef7165e489e1c633b1227aa9f509092b8
Swapnil Adhav <Swapnil_Adhav@gap.com>
2016-09-22 18:09:05 +0530

    added conflict

--- src/main/java/org/camel/process/LoggingProcessor.java
+++ src/main/java/org/camel/process/LoggingProcessor.java
@@ -4,9 +4,11 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
 public class LoggingProcessor implements Processor {
-@Override
-public void process(Exchange exchange) throws Exception {
-System.out.println("Received Order: " +
-exchange.getIn().getBody(String.class));
-}
+	@Override
+	public void process(Exchange exchange) throws Exception {
+
+		for (int i = 0; i < 4; i++) {
+
+		}
+	}
 }
